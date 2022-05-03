1519b4e47b23de1ec5352df9bb0d3e7d06538b61
Yuliya Byshko <Yuliyalevkovets1303@gmail.com>
2022-05-03 21:24:10 +0200

    Fixed owner

--- src/test/java/com/yuliya1303/config/CredentialsConfig.java
+++ src/test/java/com/yuliya1303/config/CredentialsConfig.java
@@ -2,7 +2,7 @@ package com.yuliya1303.config;
 
 import org.aeonbits.owner.Config;
 
-@Config.Sources("classpath:com/yuliya1303/config/CredentialsConfig.java")
+@Config.Sources("classpath:config/credentials.properties")
 public interface CredentialsConfig extends Config {
     String loginPassword();
 }
