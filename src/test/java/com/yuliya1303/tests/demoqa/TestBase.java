66e1f5a8790cfb162f96b24abbfd37a884c18294
Yuliya Byshko <Yuliyalevkovets1303@gmail.com>
2022-05-03 21:03:33 +0200

    Added owner

--- src/test/java/com/yuliya1303/tests/demoqa/TestBase.java
+++ src/test/java/com/yuliya1303/tests/demoqa/TestBase.java
@@ -2,8 +2,10 @@ package com.yuliya1303.tests.demoqa;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import com.yuliya1303.config.CredentialsConfig;
 import com.yuliya1303.helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -15,12 +17,15 @@ public class TestBase {
     @BeforeAll
     static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+
+        String loginPassword = config.loginPassword();
 
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("version", "100");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://" + loginPassword + "@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
